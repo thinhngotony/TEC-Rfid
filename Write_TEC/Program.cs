9250183645ff06c187d21eb8bfd3762c83542bad
unknown <thinhngo.tony@gmail.com>
2022-07-13 14:17:53 +0700

    Init project

---
+++ Write_TEC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Write_TEC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
